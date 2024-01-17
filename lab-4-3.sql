38e1962e42f6aaab72dfac55005071c3ad7b2c4f
uma vadlamani <umasai.vadlamani@kellogg.northwestern.edu>
2024-01-17 19:09:56 +0000

    4-3 done

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,4 +1,13 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
+SELECT teams.name, players.first_name, players.last_name, stats.home_runs, teams.year
+FROM stats 
+INNER JOIN teams ON stats.team_id = teams.id
+INNER JOIN players ON stats.player_id = players.id
+WHERE teams.year = 2019
+GROUP BY players.id
+ORDER BY stats.home_runs DESC
+LIMIT 1
+-- why did the limit 1 change everything??
 
 -- Expected result:
 --
